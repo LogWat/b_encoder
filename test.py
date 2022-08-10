d8feb2689c8f1936b6a32669aeea54048c84ba3a
LogWat <Nk8877wpt+Karan@gmail.com>
2022-08-10 18:43:32 +0900

    test.py: decode

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 # 与えられたバイト列から，目的のASCII範囲への変換を行うための
 # 変換式を求めるプログラム
 
-for i in range(0x1, 0xFF + 1):
-    # convert 0x1 ~ 0xFF to ASCII(0x61 ~ 0x70)
+l = []
+
+print("[!] Encode")
+for i in range(0x0, 0xFF + 1):
     b0 = i % 0x10   # 下位4bit
     b1 = i // 0x10  # 上位4bit
     b0 += 0x61      # 0x1 ~ 0xF -> 0x61 ~ 0x70
     b1 += 0x61      # 0x1 ~ 0xF -> 0x61 ~ 0x70
-    print("{:X} => {}{}".format(i, chr(b1), chr(b0)))
+    print("0x{:02X} => {}{}".format(i, chr(b1), chr(b0)))
+    l.append(chr(b1) + chr(b0))
+
+print("[!] Decode")
+for i in l:
+    r = (ord(i[0]) - 0x61 << 4) + (ord(i[1]) - 0x61)
+    print("{} => 0x{:02X}".format(i, r))
