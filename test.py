00d5d4c6ba9cb6351c7fe32ed924cd34959b2e3c
LogWat <Nk8877wpt+Karan@gmail.com>
2022-08-10 18:39:29 +0900

    test.py: f

--- test.py
+++ test.py
@@ -2,9 +2,9 @@
 # 変換式を求めるプログラム
 
 for i in range(0x1, 0xFF + 1):
-    # convert 0x1 ~ 0xFF to ASCII(0x21 ~ 0x7E)
-    a = i & 0x7F + (i & 0x80 >> 8)
-    if a > 0x7E or a < 0x21:
-        print('[!] {}: {}'.format(hex(i), hex(a)))
-    else:
-        print('[*] {}: {}'.format(hex(i), chr(a)))
+    # convert 0x1 ~ 0xFF to ASCII(0x61 ~ 0x70)
+    b0 = i % 0x10   # 下位4bit
+    b1 = i // 0x10  # 上位4bit
+    b0 += 0x61      # 0x1 ~ 0xF -> 0x61 ~ 0x70
+    b1 += 0x61      # 0x1 ~ 0xF -> 0x61 ~ 0x70
+    print("{:X} => {}{}".format(i, chr(b1), chr(b0)))
